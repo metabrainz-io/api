59adf8234a3bc03c5709ee3b7470b51e8b86c7bf
unameit10000000 <free4ms@gmail.com>
2021-12-30 19:42:00 +0100

    Updated

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-uwsgi --ini $API_ROOT/restapi/restapi.ini
+uwsgi --ini "${API_ROOT}/restapi/restapi.ini"
 service nginx reload && service nginx restart
 
 # if file exists
-tail -f $API_ROOT/logs/uwsgi-emperor.log
+tail -f "${API_ROOT}/logs/uwsgi-emperor.log"
